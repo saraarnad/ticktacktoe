11bb889a399d203a460ba4274142c9c6425f1599
guttih <gudjonholm@gmail.com>
2015-10-29 02:10:42 +0000

    nú er hægt að keyra test og fá test-report, einnig addaði ég jacoco til þess að fá codeCoverage testanna.  reportin enda í build/reports/tests.  Ég lét eitt fall vera uncoverað svo hægt sé að sjá hvernig það reportast

--- src/main/java/is/ru/ticktacktoe/TickTackToe.java
+++ src/main/java/is/ru/ticktacktoe/TickTackToe.java
@@ -5,6 +5,10 @@ public class TickTackToe {
    public static int intRet() {
         return 0;
     }
+    public static int uncovered() {
+    	System.out.println("I am an uncovered function");
+        return 0;
+    }
 
    public static void main(String[] args) {
         System.out.println("Hello TickTackToe");
