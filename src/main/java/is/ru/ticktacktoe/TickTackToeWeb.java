b3c9500ae52449deaa41d75e0d7093e9d7755e97
saraarnad <saraarn13@ru.is>
2015-11-02 02:25:56 +0000

    Added Web class for ticktacktoe

---
+++ src/main/java/is/ru/ticktacktoe/TickTackToeWeb.java
@@ -0,0 +1,33 @@
+package is.ru.ticktacktoe;
+
+import spark.*;
+import static spark.Spark.*;
+import spark.servlet.SparkApplication;
+
+public class TickTackToeWeb implements SparkApplication
+{
+	//buaa til constructor?
+	private TickTackToe game;
+
+	public static void main(String[] args)
+	{
+		staticFileLocation("/public");
+		SparkApplication TickTackToe = new TickTackToeWeb();
+		String port = System.getenv("PORT");
+		if(port != null)
+		{
+			port(Integer.valueOf(port));
+		}
+
+		//TickTackToe.init();
+	}
+
+	//@Override
+	public void init()
+	{
+		if (game == null)
+		{
+			game = new TickTackToe();
+		}
+	}
+}
